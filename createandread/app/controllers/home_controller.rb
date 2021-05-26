1edae015cbc9f45196aafcd93082d4d7435b64a8
이경민 <lienero@naver.com>
2021-05-26 17:24:27 +0900

    0526 루비온레일즈 create & read 기능 구현

--- createandread/app/controllers/home_controller.rb
+++ createandread/app/controllers/home_controller.rb
@@ -1,2 +1,18 @@
 class HomeController < ApplicationController
+    def index
+        @post = Post.all
+    end
+
+    def write
+        
+    end
+
+    def create
+        post = Post.new
+        post.title = params[:title]
+        post.content = params[:content]
+        post.save        
+
+        redirect_to '/index'
+    end
 end
--- createandread/config/routes.rb
+++ createandread/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/index' => 'home#index'
+  get '/write' => 'home#write'
+  post '/create' => 'home#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
