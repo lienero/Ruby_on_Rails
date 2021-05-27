0e5f5ed4fdbe636de85fe7b7c4496a7fd7eb8923
이경민 <lienero@naver.com>
2021-05-27 17:25:00 +0900

    0527 스캐폴딩 용어 해설(주석)

--- scaffold/app/controllers/posts_controller.rb
+++ scaffold/app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  # before_action 메서드는 액션 메서드가 실행되기 전에 실행할 메서드이다. 이러한 메서드를 필터라고 부른다.
+  # before_action method, only: action
+  # method는 필터로 실행되는 메서드, action은 필터를 적용할 액션 매서드(어레이)
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
@@ -57,6 +60,7 @@ class PostsController < ApplicationController
   end
 
   private
+    # show,edit, update, destroy 할 시 자동으로 실행
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
--- scaffold/config/routes.rb
+++ scaffold/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+# PUT : 자원의 전체 교체, 자원교체 시 모든 필드 필요)
+# PATCH : 자원의 부분 교체, 자원교체시 일부 필드 필요
+  # rake routes 명령으로 routes.rb를 분석하여 사용되고 있는 라우트 목록을 확인할 수 있다.
+  # URL                       액션    HTTP 메서드   역할
+  # /posts(.:format)          index   GET           목록 화면 표시 
+  # /posts(.:format)          show    GET           개별 상세 화면 표시 
+  # /posts/new(.:format)      new     GET           새로운 데이터 등록 화면 표시
+  # /posts(.:format)          create  POST          새로운 데이터 등록 화면의 입력을 받아 데이터 등록 처리 
+  # /posts/:id/edit(.:format) edit    GET           수정 화면 표시
+  # /posts/:id(.:format)      update  PATCH/PUT     수정 화면의 입력을 받아 수정 처리 
+  # /posts/:id(.:format)      destroy DELETE        목록 화면에서 지정된 데이터 제거 처리 
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
