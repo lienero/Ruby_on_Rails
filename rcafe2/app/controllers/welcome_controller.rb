13f7d2130325788f7ff1574f908e892c49473206
이경민 <lienero@naver.com>
2021-05-28 15:51:42 +0900

    0528 rccafe2 프로젝트 모델, 컨트롤러, 뷰 생성

---
+++ rcafe2/app/controllers/welcome_controller.rb
@@ -0,0 +1,4 @@
+class WelcomeController < ApplicationController # 모든 컨트롤러는 일차적으로 ApplicationController 클래스로부터 상속받는다.
+  def index
+  end
+end
--- rcafe2/config/routes.rb
+++ rcafe2/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # resource_route 모듈을 호출하여 config/routes.rb 파일에 resources :posts 라인을 추가한다. 이와 같이 라우팅을 선언하는 방법을 리소스 라우팅이라고 한다. 
+  resources :posts
+  # root 라우트 정의는 routes.rb 파일에서 최상위에 위치해야 한다. 
+  # 여기서 welcome 컨트롤러의 index 액션을 루트 라우트로 지정하면 홈페이지(http://localhost:3000) 접속시에 자동으로 welcome 컨트롤러의 index 액션이 호출된다.
+  # 주의할 것은 컨틀롤러와 액션명 사이에 '/'가 아니고 '#' 문자를 사용해야 한다는 것이다.
+  root 'welcome#index'
+  # get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
